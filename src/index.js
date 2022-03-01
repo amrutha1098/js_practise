c3d470a0aa1657c13a33ba717c94cba941bda2cd
amrutha <amruthar1098@gmail.com>
2022-03-01 22:35:07 +0530

    first_commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom'
+import logo from './logo.svg';
+
+function MyProfilepic(){
+  return (
+    <div>
+    <img src={logo} alt="logo" height="30px"/>
+    </div>
+  )
+}
+
+function MyInfo(){
+  return (
+    <div>
+      <h1>Amrutha</h1>
+      <h3>Front End Developer</h3>
+      <h3>Amrutha.website</h3>
+    </div>  
+  )
+}
+
+function About(){
+  return (
+    <div>
+    <h1>About</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget dolor at magna fermentum iaculis. Cras luctus hendrerit velit id tincidunt. Nunc egestas sit amet mi sed placerat. Praesent dictum porta nulla, eu dictum odio scelerisque non. Vestibulum bibendum erat mattis, lobortis libero sed, aliquet leo. Suspendisse dolor massa, faucibus in velit eget, egestas iaculis nunc. Aenean ultricies odio eget dolor pharetra, sed efficitur nisl sollicitudin.</p>  
+    </div>
+  )
+}
+
+function Interests(){
+  return (
+    <div>
+      <h3>Interests</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget dolor at magna fermentum iaculis. Cras luctus hendrerit velit id tincidunt. Nunc egestas sit amet mi sed placerat. Praesent dictum porta nulla, eu dictum odio scelerisque non. Vestibulum bibendum erat mattis, lobortis libero sed, aliquet leo. Suspendisse dolor massa, faucibus in velit eget, egestas iaculis nunc. Aenean ultricies odio eget dolor pharetra, sed efficitur nisl sollicitudin.</p>   
+ </div>
+  )
+}
+
+function Footer(){
+  return (
+    <h1>Special icons</h1>
+  )
+}
+
+function DigitlBusinesscard(){
+  return (
+   <div>
+      <MyProfilepic/>
+      <MyInfo/>
+      <About/>
+      <Interests/>
+      <Footer/>
+   </div>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+<DigitlBusinesscard/>
+,document.getElementById("root"));
